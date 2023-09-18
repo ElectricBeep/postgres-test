0f2a28e96d3ef7f5fa6620984d7bc33f738ce172
ElectricBeep <adnan.smajic4431@gmail.com>
2023-09-18 21:54:21 +0200

    db

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -21,5 +21,11 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+    ssl: true,
   },
 };
