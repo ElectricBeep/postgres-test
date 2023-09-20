7e375e10018e29b795d5890beceea40215f9b49a
ElectricBeep <adnan.smajic4431@gmail.com>
2023-09-20 22:36:19 +0200

    changes

--- client/app/components/UsersList.tsx
+++ client/app/components/UsersList.tsx
@@ -1,31 +1,48 @@
+"use client";
+
 import { User } from "@/types";
-import { FC } from "react"
+import axios from "axios";
+import { useState } from "react";
 
-interface UsersListProps {
-  users: User[]
-}
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-const UsersList: FC<UsersListProps> = ({ users }) => {
-  if (users.length === 0) {
-    return (
-      <div className="mx-auto w-fit">
-        Gettings users...
-      </div>
-    )
+  const getUsers = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(`${process.env.BASE_URL}/api/users`);
+      setUsers(res.data);
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
+
   return (
-    <>
+    <div className="flex flex-col gap-5 items-center justify-center">
+      <button
+        onClick={getUsers}
+        disabled={isLoading}
+        className="bg-black text-white text-bold px-4 py-2 rounded-lg"
+      >
+        {isLoading ? "Loading..." : "Get Users"}
+      </button>
       <div>
-        {users?.map((user) => (
-          <div key={user.id} className="mx-auto w-fit mt-4">
-            <div> ID: {user.id}</div>
-            <div>NAME: {user.name}</div>
-            <div>EMAIL: {user.email}</div>
-            <div>CREATED: {user.createdAt}</div>
-          </div >
-        ))}
+        {users.length === 0
+          ? <div className="mx-auto w-fit">{isLoading ? "Loading..." : ""}</div>
+          : users?.map((user: User) => (
+            <div key={user.id} className="mx-auto w-fit mt-4">
+              <div> ID: {user.id}</div>
+              <div>NAME: {user.name}</div>
+              <div>EMAIL: {user.email}</div>
+              <div>CREATED: {user.createdAt}</div>
+            </div>
+          ))}
       </div >
-    </>
+    </div>
   )
 }
 
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,24 +1,9 @@
-import axios from "axios"
 import UsersList from "./components/UsersList";
-import { User } from "@/types";
 
 export default async function Home() {
-  const getUsers = async (): Promise<User[]> => {
-    try {
-      const res = await axios.get(`${process.env.BASE_URL}/api/users`);
-      return (res.data as any) || [];
-
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  }
-
-  const users = await getUsers();
-
   return (
     <div className="w-[1440px] mx-auto py-10">
-      <UsersList users={users} />
+      <UsersList />
     </div>
   )
 }
